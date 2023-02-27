37d31399e63e4040851c8a4ad117a46622c4a17a
denisaverchenkov <Den6611@gmail.com>
2023-02-27 17:37:11 +0400

    Add some delays

---
+++ src/test/java/org/example/CalculatorTestWithDelays3.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class CalculatorTestWithDelays3 {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() throws InterruptedException {
+        Thread.sleep(10000);
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    public void testSubtract() throws InterruptedException {
+        Thread.sleep(10000);
+        assertEquals(2, calculator.subtract(4, 2));
+    }
+
+    @Test
+    public void testMultiply() throws InterruptedException {
+        Thread.sleep(10000);
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() throws InterruptedException {
+        Thread.sleep(10000);
+        assertEquals(2, calculator.divide(4, 2));
+    }
+
+    @Test
+    public void testDivideByZero() throws InterruptedException {
+        Thread.sleep(10000);
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(4, 0));
+    }
+}
