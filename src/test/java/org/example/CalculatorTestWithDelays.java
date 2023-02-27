1c59c3123d97f69429a9e087e97e959d6f3ee2ca
denisaverchenkov <Den6611@gmail.com>
2023-02-27 16:51:09 +0400

    Second method

--- src/test/java/org/example/CalculatorTestWithDelays.java
+++ src/test/java/org/example/CalculatorTestWithDelays.java
@@ -1,36 +1,40 @@
 package org.example;
 
-import org.example.Calculator;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
 
-public class CalculatorTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class CalculatorTestWithDelays {
     private Calculator calculator = new Calculator();
 
     @Test
     public void testAdd() throws InterruptedException {
+        Thread.sleep(10000);
         assertEquals(4, calculator.add(2, 2));
     }
 
     @Test
-    public void testSubtract() {
+    public void testSubtract() throws InterruptedException {
+        Thread.sleep(10000);
         assertEquals(2, calculator.subtract(4, 2));
     }
 
     @Test
     public void testMultiply() throws InterruptedException {
+        Thread.sleep(10000);
         assertEquals(6, calculator.multiply(2, 3));
     }
 
     @Test
-    public void testDivide() {
+    public void testDivide() throws InterruptedException {
+        Thread.sleep(10000);
         assertEquals(2, calculator.divide(4, 2));
     }
 
     @Test
-    public void testDivideByZero() {
+    public void testDivideByZero() throws InterruptedException {
+        Thread.sleep(10000);
         assertThrows(IllegalArgumentException.class, () -> calculator.divide(4, 0));
     }
 }
-
-
