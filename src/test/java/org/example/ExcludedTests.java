483c2ce12962e6a6e56b65acd0a8e07f2028d262
denisaverchenkov <Den6611@gmail.com>
2023-03-06 13:08:29 +0400

    excluded tests

--- src/test/java/org/example/ExcludedTests.java
+++ src/test/java/org/example/ExcludedTests.java
@@ -5,33 +5,36 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
-public class CalculatorTest_1 {
+public class ExcludedTests {
     private Calculator calculator = new Calculator();
 
     @Test
     public void testAdd() throws InterruptedException {
+        Thread.sleep(10000);
         assertEquals(4, calculator.add(2, 2));
     }
 
     @Test
-    public void testSubtract() {
+    public void testSubtract() throws InterruptedException {
+        Thread.sleep(10000);
         assertEquals(2, calculator.subtract(4, 2));
     }
 
     @Test
     public void testMultiply() throws InterruptedException {
-        assertEquals(6, calculator.multiply(2, 3));
+        Thread.sleep(10000);
+        assertEquals(7, calculator.multiply(2, 3));
     }
 
     @Test
-    public void testDivide() {
+    public void testDivide() throws InterruptedException {
+        Thread.sleep(10000);
         assertEquals(2, calculator.divide(4, 2));
     }
 
     @Test
-    public void testDivideByZero() {
+    public void testDivideByZero() throws InterruptedException {
+        Thread.sleep(10000);
         assertThrows(IllegalArgumentException.class, () -> calculator.divide(4, 0));
     }
 }
-
-
